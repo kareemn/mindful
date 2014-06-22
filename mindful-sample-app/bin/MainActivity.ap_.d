008ec064fe237296c499b131759148972c6ffaa0
Kareem <knassar89@gmail.com>
2014-06-22 16:36:29 -0700

    mindful sample app and jarwq

---
+++ mindful-sample-app/bin/MainActivity.ap_.d
@@ -0,0 +1,19 @@
+/Users/knassar/Documents/workspace/MindfulTestApp/bin/MainActivity.ap_ \
+ : /Users/knassar/Documents/workspace/MindfulTestApp/res/drawable-hdpi/ic_launcher.png \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/drawable-mdpi/ic_launcher.png \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/drawable-xhdpi/ic_launcher.png \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/drawable-xxhdpi/ic_launcher.png \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/layout/activity_main.xml \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/menu/main.xml \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/values/dimens.xml \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/values/strings.xml \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/values/styles.xml \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/values-sw600dp/dimens.xml \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/values-sw720dp-land/dimens.xml \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/values-v11/styles.xml \
+/Users/knassar/Documents/workspace/MindfulTestApp/res/values-v14/styles.xml \
+/Users/knassar/Documents/workspace/MindfulTestApp/bin/res/drawable-hdpi/ic_launcher.png \
+/Users/knassar/Documents/workspace/MindfulTestApp/bin/res/drawable-mdpi/ic_launcher.png \
+/Users/knassar/Documents/workspace/MindfulTestApp/bin/res/drawable-xhdpi/ic_launcher.png \
+/Users/knassar/Documents/workspace/MindfulTestApp/bin/res/drawable-xxhdpi/ic_launcher.png \
+/Users/knassar/Documents/workspace/MindfulTestApp/bin/AndroidManifest.xml \
